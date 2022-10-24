03f30e17e5f333163618d487d08c3a4acd4815a4
Daniel <daniel@ubuntu.local>
2022-10-24 13:16:58 +0200

    Add soundcheck, attempt #1

--- soundcheck.py
+++ soundcheck.py
@@ -17,13 +17,23 @@ config = None
 # sanity checks
 # -------------------------------------------------------------------
 
+print("-------------------------------------------------------------------")
+print("Soundcheck")
+print("-------------------------------------------------------------------")
+print("")
+
 with open(config_file, 'r') as file:
     config = yaml.safe_load(file)
 
 if config['soundFile'] is None:
-    logger.error("Missing sound file reference, cannot continue!")
+    print(" ❌ Missing sound file reference, cannot continue!")
 
     sys.exit(1)
 
+print(" ✅ Configuration OK")
+print("Playing sound ...")
+
 sound = Sound()
 sound.play_mp3(config['soundFile'])
+
+print(" ✅ Done playing sound. Did you hear it?")
